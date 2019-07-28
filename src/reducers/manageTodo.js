1284e44bd1c574f2c42c35de80a62c0f543c965e
Lara Sanders <larasanders@gmail.com>
2019-07-28 20:42:17 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -8,11 +8,11 @@ export default function manageTodo(state = {
       id: Math.random()*10000000000000000,
       text: action.payload.text
     }
-    return { todos: state.todos.concat(todo) };
+      return { todos: state.todos.concat(todo) };
 
     case 'DELETE_TODO':
 
-      return {todos: state.todos.filter(todo => todo !== action.payload)}
+      return {todos: state.todos.filter(todo => todo.id !== action.payload)}
 
     default:
       return state;
